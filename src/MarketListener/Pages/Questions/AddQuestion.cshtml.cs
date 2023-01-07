0101fccd85d2cfcb048e4b07396e2e4f25cce615
Navid <navidroshdieh@gmail.com>
2023-01-07 17:06:49 +0330

    feat: add Question

--- src/MarketListener/Pages/Questions/AddQuestion.cshtml.cs
+++ src/MarketListener/Pages/Questions/AddQuestion.cshtml.cs
@@ -1,3 +1,6 @@
+using MarketListener.Application.Features.Question.Commands;
+using MarketListener.ViewModels.Question;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,37 @@ namespace MarketListener.Pages.Questions
 {
     public class AddQuestionModel : PageModel
     {
+        private readonly IMediator _mediator;
+        AddQuestionVM addQuestionVM; 
+
+        public AddQuestionModel(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await _mediator.Send(new AddQuestionCommand()
+                {
+                    Title = addQuestionVM.Title,
+                    Text = addQuestionVM.Text,
+                    QuestionType = addQuestionVM.QuestionType,
+                    IsTimeLimited = addQuestionVM.IsTimeLimited,
+                    TimeLimitSeconds = addQuestionVM.TimeLimitSeconds                    
+                });
+
+                return RedirectToPage("Questions"); 
+            }
+
+
+            return Page();
+
+
         }
     }
 }
---
+++ src/MarketListener/ViewModels/Question/AddQuestionVM.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketListener.ViewModels.Question;
+
+using MarketListener.Domain.Enums;
+
+public class AddQuestionVM
+{
+    public string Title { get; set; } = default!;
+    public string Text { get; set; } = default!;
+    public QuestionType QuestionType { get; set; }
+    public bool IsTimeLimited { get; set; }
+    public int TimeLimitSeconds { get; set; }
+    public int CurrentUserId { get; set; }
+}
